f3df514ea6169e40f4ef1018323bad8dafab8a53
Travis-Simmons <920117874@student.ccga.edu>
2022-04-13 11:00:25 -0400

    comments and cleaning

--- session2/scripts_section/arguments_using_argparse.py
+++ session2/scripts_section/arguments_using_argparse.py
@@ -1,10 +1,13 @@
 import argparse
 
+# Setup Arguments
 parser = argparse.ArgumentParser(description='Process some integers.')
 
+# parser.add_argment('-{command line character}', '--{variable in script}', type = {str, int, float}, help = 'a message to display')
 parser.add_argument('-n','--number', type = int, help='a number')
 
 args = parser.parse_args()
 
+#------------------------------------------------------------------------------------------------------------------------------------ 
 
 print('You can use any argmuent like: ', args.number) 
--- session2/scripts_section/read_csv.py
+++ session2/scripts_section/read_csv.py
@@ -4,13 +4,11 @@ import argparse
 # Setup Arguments
 parser = argparse.ArgumentParser(description='Process some integers.')
 
+# parser.add_argment('-{command line character}', '--{variable in script}', type = {str, int, float}, help = 'a message to display')
 parser.add_argument('-c','--csv', type = str, help='Path to a csv')
 
 args = parser.parse_args()
-
-
-
-
+# -----------------------------------------------------------------------------------------
 
 # Example from notebook
 df = pd.read_csv(args.csv)
