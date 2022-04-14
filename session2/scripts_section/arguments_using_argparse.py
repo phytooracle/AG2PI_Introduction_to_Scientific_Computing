27aaf2f314858ccebdbdaac1020be31bea3fc4ae
Travis-Simmons <920117874@student.ccga.edu>
2022-04-14 12:30:41 -0400

    changing to hello, argument

--- session2/scripts_section/arguments_using_argparse.py
+++ session2/scripts_section/arguments_using_argparse.py
@@ -3,11 +3,18 @@ import argparse
 # Setup Arguments
 parser = argparse.ArgumentParser(description='Process some integers.')
 
-# parser.add_argment('-{command line character}', '--{variable in script}', type = {str, int, float}, help = 'a message to display')
-parser.add_argument('-n','--number', type = int, help='a number')
+# parser.add_argment('-{command line character}', 
+#                    '--{variable in script}', 
+#                      type = {str, int, float}, 
+#                       help = 'a message to display')
+
+parser.add_argument('-n',
+                    '--name', 
+                    type = str, 
+                    help='a name')
 
 args = parser.parse_args()
 
 #------------------------------------------------------------------------------------------------------------------------------------ 
 
-print('You can use any argmuent like: ', args.number) 
+print('Hello', args.name) 
