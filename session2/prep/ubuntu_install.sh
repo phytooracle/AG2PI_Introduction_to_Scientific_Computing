91515ffbac5a50a0ac22a93a113ece71d2b9f678
emmanuelgonz <emmanuelgonzalez@email.arizona.edu>
2022-04-14 23:50:49 -0700

    remove pips

--- session2/prep/ubuntu_install.sh
+++ session2/prep/ubuntu_install.sh
@@ -6,4 +6,3 @@ sudo apt-get update
 sudo apt-get install python3.8
 wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
 bash Miniconda3-latest-Linux-x86_64.sh
-#python3.8 -m pip install matplotlib scipy sklearn pandas numpy
